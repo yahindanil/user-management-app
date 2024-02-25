b7f6040e441e131b8227859a59f7b7ea4186ce82
yahindanil <ahin.danil@gmail.com>
2024-02-25 21:41:27 +0400

    Updates

---
+++ static/js/952.a7d7c66f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkuser_management_app=self.webpackChunkuser_management_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.a7d7c66f.chunk.js.map
